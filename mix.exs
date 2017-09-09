ede03ac45a63d07e5c5728867b7949b92d3cf450
Kevin Mannix <kmannix@bu.edu>
2017-09-09 14:41:18 -0400

    pow -> pattern mathing

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule Pow.Mixfile do
+defmodule PatternMathing.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :pow,
+      app: :pattern_mathing,
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
